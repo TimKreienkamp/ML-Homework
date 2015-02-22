0f636f78112756407a7a135bb6dbc8c1b994a31f
TimKreienkamp <tim.kreienkamp@gmail.com>
2015-02-22 18:35:51 +0100

    small bugfix

--- Model_Selection/sim_data.py
+++ Model_Selection/sim_data.py
@@ -14,16 +14,7 @@ def create_pos_biased(x):
         bool_1 = (x > intervals[row,0] and x <= intervals[row,1])
     return bool_1
 create_pos_biased = np.vectorize(create_pos_biased)
-
-                     
-def apply_bias(x):
-    if x:
-        x = 0.5+bias
-    else:
-        x = 0.5-bias
-    return x    
-apply_bias = np.vectorize(apply_bias)
-       
+   
 def apply_binom(x):
     y = numpy.random.binomial(1, x)
     return y   
@@ -33,10 +24,10 @@ apply_binom = np.vectorize(apply_binom)
 def sim_data(intervals, ndraws, bias):
      x = numpy.random.uniform(0,1,ndraws)
      pos_biased = create_pos_biased(x)
-     eta_x = apply_bias(pos_biased)
+     eta_x = np.where(pos_biased, 0.5+bias, 0.5-bias)
      y = apply_binom(eta_x)
      return (y,x)
      
-y, x = sim_data(intervals, 10**8, 0.3)
+y, x = sim_data(intervals, 10**5, 0.)
     
 print(y)
