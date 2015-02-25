3fb72135ab9623ee53643dde34a8f23a966c46e5
TimKreienkamp <tim.kreienkamp@gmail.com>
2015-02-25 13:01:55 +0100

    done

--- Model_Selection/RWrapper.R
+++ Model_Selection/RWrapper.R
@@ -1,4 +1,6 @@
-interval_selection <- function(bias, n){
+setwd("/Users/timkreienkamp/documents/studium/data_science/machine_learning/problem_sets/ML-Homework/model_selection/")
+
+interval_selection <- function(bias = 0.2, n_train = 1000, n_val=500, n_intervals = 100, shortest_k = 3){
   
   if (!require("ggplot2")) install.packages("ggplot2")
   if (!require("rPython")) install.packages("rPython")
@@ -14,98 +16,49 @@ interval_selection <- function(bias, n){
   python.exec('import numpy as np')
   python.exec('from collections import deque')
   python.exec('from copy import copy')
-  python.exec("def _sim_data(intervals, ndraws, bias):
-  x = np.random.uniform(0, 1, ndraws)
-  is_in_int = np.vectorize(lambda x:
-                            np.any(np.apply_along_axis(lambda int: int[0] <= x <= int[1], 1, intervals)))
-  y = np.where(
-    is_in_int(x), 
-    np.random.binomial(1, 0.5 + bias, ndraws), 
-    np.random.binomial(1, 0.5 - bias, ndraws)
-    )
-  return(np.sort(x), y[np.argsort(x)])")
-
+  python.load("struct.py")
+  python.load("algo.py")
+  python.load("extract_intervals.py")
   
-  python.exec('def selection(y, nint, save_all = True):
-  
-  classifiers = []
-  g = _init(y)
-  
-  if len(g) % 2 != nint % 2:
-      if g[1]["adv"] > g[-1]["adv"]:
-            g[-2]["adv"] -= g[-1]["adv"]
-            g[-2]["cutoff"] = g[-1]["cutoff"]
-            g.pop()
-      else:
-            g[1]["adv"] -= g[0]["adv"]
-            g.popleft()
-  
-  (save_all and classifiers.append(copy(g)))
-  
-  all((_dp(g), save_all and classifiers.append(copy(g))) for i in range((len(g) - nint) / 2))
-  
-  
-
-  return(save_all and classifiers or [g])')
-  
-  python.exec('def _init(y):
-    g_n = deque()
-    g_n.append({"cutoff": 0, "label": y[0], "adv": 1})
-    for i in range(1, len(y)):
-      if y[i] == g_n[-1]["label"]:
-        g_n[-1]["cutoff"] += 1
-        g_n[-1]["adv"] += 1
-      else:
-          g_n.append({"cutoff": i, "label": y[i], "adv": 1})
-    return g_n')
-  
-  python.exec('def _dp(g):
-    
-    outer_adv = g[1]["adv"] + g[-1]["adv"]
-    inner_adv = [interval["adv"] for interval in g]
-    del inner_adv[-1], inner_adv[0]
-  
-    if outer_adv > np.max(inner_adv):
-      g.rotate(-(np.argmin(inner_adv) + 1))
-      g[-1]["adv"] = g[-1]["adv"] - g[0]["adv"] + g[1]["adv"]
-      g[-1]["cutoff"] = g[1]["cutoff"]
-      (g.popleft(), g.popleft(),  g.rotate(np.argmin(inner_adv) + 1))
-    else:
-        g[1]["adv"] -= g[0]["adv"]
-        g[-2]["adv"] -= g[-1]["adv"]
-        g[-2]["cutoff"] = g[-1]["cutoff"]
-        (g.pop(), g.popleft())')
-  python.exec('def predict(pred_intervals, x_val):
-      is_in_int = np.vectorize(lambda x:
-          np.any(np.apply_along_axis(lambda int: int[0] <= x <= int[1], 1, pred_intervals)))
-      y_pred = float(is_in_int(x_val))
-      return y_pred')
- 
-  ####################################
-  #python function execution
-  #####################################
-  
+  #variable assignments
   python.assign("bias", bias)
-  python.assign("ndraws", n)
+  python.assign("n_train", n_train)
+  python.assign("n_val", n_train)
+  python.assign("shortest_k", shortest_k)
   
-  python.exec("intervals = np.array([[0.1,0.5],[0.9,1.0]])")
-  python.exec("x,y = _sim_data(intervals, ndraws,bias)")
-  python.exec("y = y.tolist()")
-  python.exec("x = x.tolist()")
-  y = python.get("y")
-  x = python.get("x")
+  partition_size = 1/n_intervals
+  python.assign("partition_size", partition_size)
   
-
-  
-  
-  ######################################
-  #collection everything back into R
-  ######################################
-  out_list = list(x=x,y=y)
-  python.exec("classifiers = selection(y,4)")
-  python.exec("classifiers = classifiers")
-  out_list = list(x=x,y=y)
-  return(out_list)
+  python.exec("true_intervals = _get_true_intervals(partition_size)")
+  python.exec("x_train,y_train = _sim_data(true_intervals, n_train, bias)")
+  python.exec("x_val,y_val = _sim_data(true_intervals, n_val, bias)")
+  python.exec("y = y_train")
+  python.exec("p = Partition(y_train)")
+  python.exec("classifiers_even = p.select(3,save = True)")
+  python.exec(("p = Partition(y_train)"))
+  python.exec("classifiers_odd = p.select(4,save = True)")
+  python.exec("classifiers = _zip_lists(classifiers_odd, classifiers_even)")
+  python.exec("all_intervals = _get_all_intervals(classifiers, x_train)")
+  python.exec("validation_error, training_error, complexity = _get_all_errors(all_intervals, x_val, x_train, y_val,y_train, classifiers, shortest_k)")
+  validation_error = python.get("validation_error")
+  training_error = python.get("training_error")
+  complexity = python.get("complexity")
+  error_type <- c((rep("Training", length(training_error))), rep("Validation", length(validation_error)))
+  error <- c(training_error, validation_error)
+  complexity <- c(complexity, complexity)
+  results <- data.frame(error_type,complexity, error,stringsAsFactors=FALSE)
+  plot <- ggplot(data = results, aes(x = complexity, y= error, colour = error_type, group = error_type)) +  geom_line()  + ggtitle("Validation/Training Error vs Complexity")
+  return(list(results=results, plot=plot))
 }
-interval_selection(0.2,10)
+
+ggplot(data = results$results, aes(x = complexity, y= Error, colour = error_type, group = error_type)) +  geom_line()
+
+results <- interval_selection(n_train = 10000, shortest_k = 70)
+plot1 <- results$plot + ggtitle ("Validation /Training Error vs Complexity (Bias = 0.2)")
+results_2 <- interval_selection(n_train = 10000, shortest_k = 70, bias = 0.4)
+
+plot2 <- results_2$plot + ggtitle ("Validation /Training Error vs Complexity (Bias = 0.4)")
+
+
+
 
